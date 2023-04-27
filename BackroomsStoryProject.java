cd1894f6148ef26f94e28118a1c37e937f1f6d4c
MarcoRodriguez935 <120834968+MarcoRodriguez935@users.noreply.github.com>
2023-04-27 14:22:58 -0700

    Update BackroomsStoryProject.java

--- BackroomsStoryProject.java
+++ BackroomsStoryProject.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class BackroomsStoryProject {
 	public static void main(String[] args) {
+		introMessage();
 		displayTitle();
+
 	}
 	
 	public static void displayTitle() {
@@ -20,5 +22,25 @@ public class BackroomsStoryProject {
 		System.out.println("                               ░         ░                                                           ");
 
 		System.out.println();
+	}
+
+	public static void introMessage () {
+		Scanner input = new Scanner (System.in);
+
+		System.out.print("Enter your name: ");
+		String name = input.nextLine();
+		name = name.trim();
+
+		System.out.println();
+		System.out.println("Hello " + name);
+		System.out.println("You were having a normal day when you tripped and miraculously noclipped through " +
+			"the ground in broad daylight.");
+		System.out.println("You are now in an unknown place.\nYou see nothing ahead but an endless number " +
+			"of seemingly randomly generated rooms.\nThis place does not seem real, yet you stand here in disbelief.");
+		System.out.println("Are you alone? Perhaps stuck here for eternity? Only time can tell.");
+		System.out.println("Still, there's no guarantee time passes normally here.");
+		System.out.println("Welcome to.......");
+		System.out.println();
+
 	}
 }
