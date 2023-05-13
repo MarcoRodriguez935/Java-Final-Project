dd3b0972dd23cf50961af25cd6a715703f764db6
MarcoRodriguez935 <120834968+MarcoRodriguez935@users.noreply.github.com>
2023-05-12 21:29:04 -0700

    Update InventoryMethods.java

--- InventoryMethods.java
+++ InventoryMethods.java
@@ -2,7 +2,61 @@ import java.util.Scanner;
 
 public class InventoryMethods {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
 		String[] inventory = {"FISTS", "--EMPTY--", "--EMPTY--", "--EMPTY--", "--EMPTY--"};
+		int health = 100;
+
+		System.out.print("> ");
+		String cmd = input.nextLine();
+		cmd = cmd.trim();
+
+		if (cmd.equalsIgnoreCase("inventory"))
+			inventory(inventory, health);
+
+		System.out.println("out of inventory method");
+	}
+
+	public static String[] inventory(String[] inventory, int health) {
+		Scanner input = new Scanner(System.in);
+		boolean inv = true;
+
+		while (inv) {
+			System.out.println("\nInventory:");
+			displayList(inventory);
+			System.out.println();
+			System.out.println("What would you like to do?:\n");
+			System.out.println("Add");
+			System.out.println("Drop");
+			System.out.println("Use");
+			System.out.println("Exit");
+			System.out.println();
+
+			System.out.print("> ");
+			String cmd = input.nextLine();
+			cmd = cmd.trim();
+
+			while (!cmd.equalsIgnoreCase("add") && !cmd.equalsIgnoreCase("drop")
+				&& !cmd.equalsIgnoreCase("use") && !cmd.equalsIgnoreCase("exit")) {
+				System.out.print("> ");
+				cmd = input.nextLine();
+				cmd = cmd.trim();
+			}
+
+			if (cmd.equalsIgnoreCase("add")) {
+				addToInventory(inventory);
+			}
+			else if (cmd.equalsIgnoreCase("drop")) {
+				removeFromInventory(inventory);
+			}
+			else if (cmd.equalsIgnoreCase("use")) {
+				System.out.println("invoke heal method here");
+			}
+			else if (cmd.equalsIgnoreCase("exit")) {
+				inv = false;
+			}
+		}
+		return inventory;	
 	}
 
 	public static void displayList(String[] list) {
@@ -11,29 +65,63 @@ public class InventoryMethods {
 		}
 	}
 
-	public static String[] addToInventory(String[] inv, String addition) {
-		for (int i = 0; i < inv.length; i++) {
-			if (inv[i].equals("--EMPTY--")) {
-				inv[i] = addition;
-				break;
-			}
-		}
-		return inv;
-	}
+	public static String[] addToInventory(String[] inv) {
+		Scanner input = new Scanner(System.in);
 
-	public static String[] removeFromInventory(String[] inv, String remove) {
+		System.out.println("To exit, enter BACK\n");
 
-		if (remove.equals("FISTS")) {
-			System.out.println("Cannot drop item: FISTS");
-		}
+		System.out.print("Enter an item to add: ");
+		String addition = input.nextLine();
+		addition = addition.trim();
+
+		if (addition.equalsIgnoreCase("back"))
+			return inv;
 		else {
+			// verify if the item is valid or if the item is available for pickup
+			while (addition.equalsIgnoreCase("fists") || addition.equalsIgnoreCase("--empty--")) {
+				System.out.println("Cannot add item");
+				System.out.print("\nEnter an item to add: ");
+				addition = input.nextLine();
+				addition = addition.trim();
+			}
+
 			for (int i = 0; i < inv.length; i++) {
-				if (inv[i].equalsIgnoreCase(remove)) {
-					inv[i] = "--EMPTY--";
+				if (inv[i].equals("--EMPTY--")) {
+					inv[i] = addition;
 					break;
 				}
 			}
 		}	
+		return inv;
+	}
+
+	public static String[] removeFromInventory(String[] inv) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("To exit, enter BACK\n");
+
+		System.out.print("Enter an item to drop: ");
+		String removal = input.nextLine();
+		removal = removal.trim();
+
+		if (removal.equalsIgnoreCase("back"))
+			return inv;
+		else {
+			// verify if the item is valid or present in inventory
+			while (removal.equalsIgnoreCase("fists") || removal.equalsIgnoreCase("--empty--")) {
+				System.out.println("Cannot drop item");
+				System.out.print("\nEnter an item to drop: ");
+				removal = input.nextLine();
+				removal = removal.trim();
+			}
+
+			for (int i = 0; i < inv.length; i++) {
+				if (inv[i].equalsIgnoreCase(removal)) {
+					inv[i] = "--EMPTY--";
+					break;
+				}
+			}
+		}		
 		return inv;
 	}
 }
